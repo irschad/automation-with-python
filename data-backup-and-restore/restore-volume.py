973a03e5adec487e13524a80cc4bc5ec7ad185ca
Irshad Nizami <irniz@rediffmail.com>
2024-11-29 15:24:01 +0530

    Add restore volume code

---
+++ data-backup-and-restore/restore-volume.py
@@ -0,0 +1,57 @@
+import boto3
+from operator import itemgetter
+
+ec2_client = boto3.client('ec2', region_name="us-east-1")
+ec2_resource = boto3.resource('ec2', region_name="us-east-1")
+
+instance_id = "i-04e7a8ac01a367cce"
+
+volumes = ec2_client.describe_volumes(
+    Filters=[
+        {
+            'Name': 'attachment.instance-id',
+            'Values': [instance_id]
+        }
+    ]
+)
+
+instance_volume = volumes['Volumes'][0]
+
+snapshots = ec2_client.describe_snapshots(
+    OwnerIds=['self'],
+    Filters=[
+        {
+            'Name': 'volume-id',
+            'Values': [instance_volume['VolumeId']]
+        }
+    ]
+)
+
+latest_snapshot = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)[0]
+print(latest_snapshot['StartTime'])
+
+new_volume = ec2_client.create_volume(
+    SnapshotId=latest_snapshot['SnapshotId'],
+    AvailabilityZone="us-east-1a",
+    TagSpecifications=[
+        {
+            'ResourceType': 'volume',
+            'Tags': [
+                {
+                    'Key': 'Environment',
+                    'Value': 'dev'
+                }
+            ]
+        }
+    ]
+)
+
+while True:
+    vol = ec2_resource.Volume(new_volume['VolumeId'])
+    print(vol.state)
+    if vol.state == 'available':
+        ec2_resource.Instance(instance_id).attach_volume(
+            VolumeId=new_volume['VolumeId'],
+            Device='/dev/xvdb'
+        )
+        break
